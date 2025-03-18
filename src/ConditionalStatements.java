521023a1b8ce141e26be20c156458e76c34c3938
Bhavana Gandavalli <bhavana.tiruma09@gmail.com>
2025-03-18 22:53:42 +0000

    conditional statements

--- src/ConditionalStatements.java
+++ src/ConditionalStatements.java
@@ -12,17 +12,14 @@ public class ConditionalStatements {
             System.out.println("Weird");
 
         }
-            if(n%2 == 0 )
-            {
-                if (n>=2 && n<=5){
+            if(n%2 == 0 ) {
+                if (n >= 2 && n <= 5) {
                     System.out.println("Not Weird");
                 }
 
-                if(n>=6 && n<=20){
+                if (n >= 6 && n <= 20) {
                     System.out.println("Weird");
-                }
-
-                else if (n>20) {
+                } else if (n > 20) {
 
                     System.out.println("Not Weird");
                 }
